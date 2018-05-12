e23e88edd4471d9b0af366c3a2c49889348e644a
BlenderBro <vlad.s.dobrescu@gmail.com>
2018-05-12 13:54:00 +0300

    am populat lista cu date din json local

--- assets/js.js
+++ assets/js.js
@@ -1,9 +1,21 @@
-fetch('./db/masini.json')
+const lista = document.getElementById('lista');
+console.log(lista)
 
+fetch('./db/masini.json')
     .then(function (response) {
         return response.json();
-
     })
     .then(function (masini) {
-        console.log(masini)
+        // console.log(masini)
+        //parcurgem array-ul
+        //sa luam fiecare obiect individula si sa ii postam vaoarea cheii in DOM
+        for (let i = 0; i < 6; i++) {
+            // lista.innerHTML = '<li class="list-group-item">NumeMasina <strong> Capacitate</strong></li>'
+            console.log(masini[i].nume +' are capacitatea de: '+masini[i].capacitate );
+            lista.innerHTML += `
+            <li class="list-group-item">
+                ` + masini[i].nume +` are capacitatea de: `+ masini[i].capacitate +`
+            </li>  
+            `
+        }
     })
