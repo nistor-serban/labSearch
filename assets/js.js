bdb5c576e880f92b055da1fea159acbe5bc64e9c
nistor-serban <vasile.serban.nistor@gmail.com>
2018-05-12 14:53:52 +0300

    am schimbat ceva la search

--- assets/js.js
+++ assets/js.js
@@ -1,5 +1,5 @@
 const lista = document.getElementById('lista');
-console.log(lista)
+// console.log(lista)
 
 fetch('./db/masini.json')
     .then(function (response) {
@@ -11,11 +11,28 @@ fetch('./db/masini.json')
         //sa luam fiecare obiect individula si sa ii postam vaoarea cheii in DOM
         for (let i = 0; i < 6; i++) {
             // lista.innerHTML = '<li class="list-group-item">NumeMasina <strong> Capacitate</strong></li>'
-            console.log(masini[i].nume +' are capacitatea de: '+masini[i].capacitate );
+            // console.log(masini[i].nume +' are capacitatea de: '+masini[i].capacitate );
             lista.innerHTML += `
             <li class="list-group-item">
                 ` + masini[i].nume +` are capacitatea de: `+ masini[i].capacitate +`
             </li>  
             `
         }
+    })
+    const searchBar = document.getElementById('searchBar')
+    searchBar.addEventListener('keyup', function(event){
+        // console.log(event.target.value)
+        let term = event.target.value.toLowerCase();
+        let items = document.getElementsByTagName('li')
+        // console.log(items)
+        Array.from(items).forEach(function(item) {
+            console.log(item);
+            let nameOfItem = item.firstElementChild.textContent;
+        if(nameOfItem.toLowerCase().indexOf(term) = -1){
+            item.style.display='none';
+        } else{
+            item.style.display='block';
+        }
+        })
+        
     })
