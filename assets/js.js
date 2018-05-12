314937b86b8f79ff022765cf67e9c2af0a9eca1e
BlenderBro <vlad.s.dobrescu@gmail.com>
2018-05-12 16:11:25 +0300

    am facut ceva, am uitat ce

--- assets/js.js
+++ assets/js.js
@@ -1,20 +1,20 @@
 const lista = document.getElementById('lista');
 // console.log(lista)
 
-fetch('./db/masini.json')
+fetch('https://api.punkapi.com/v2/beers?per_page=80')
     .then(function (response) {
         return response.json();
     })
     .then(function (masini) {
-        // console.log(masini)
+        console.log(masini)
         //parcurgem array-ul
         //sa luam fiecare obiect individula si sa ii postam vaoarea cheii in DOM
-        for (let i = 0; i < 6; i++) {
+        for (let i = 0; i < masini.length; i++) {
             // lista.innerHTML = '<li class="list-group-item">NumeMasina <strong> Capacitate</strong></li>'
             // console.log(masini[i].nume +' are capacitatea de: '+masini[i].capacitate );
             lista.innerHTML += `
             <li class="list-group-item">
-                ` + masini[i].nume +` are capacitatea de: `+ masini[i].capacitate +`
+                ` + masini[i].name +` => `+ masini[i].tagline +`
             </li>  
             `
         }
